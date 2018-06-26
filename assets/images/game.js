1a82280a75da79569abf6a395d39c11b6770d920
Camille Sierra <camillemsierra@gmail.com>
2018-06-26 15:33:25 -0400

    updating homework

--- assets/images/game.js
+++ assets/images/game.js
@@ -1,27 +1,14 @@
-// array of words being guessed
+
+
 var animalList = [
     "dolphin", "monkey", "elephant", "leopard", "turtle", "kangaroo", "cheetah", "goose", "parrot",
     "crocodile"
 ];
-
-//  Computer selected random word will be held here.
 var animal = "";
-
-
-// This will break the solution into individual letters to be stored in array.
-var lettersInAnimal = [];
-
-// This will be the number of blanks we show based on the number of letters.
-var correct = 0;
-
-var word = document.getElementById("word")
-
-var started = false;
-
-// Game counters
 var incorrect = 4;
+var started = false;
 var losses = 1;
-
+var correct = 0;
 
 
 document.onkeyup = function (e) {
@@ -29,30 +16,21 @@ document.onkeyup = function (e) {
     var userKey = e.key;
     console.log(e)
 
-    //  starts the game using the spacebar 
     if (userGuess === 32 && !started) {
         started = true;
-        //  generates a random word
         animal = animalList[Math.floor(Math.random() * animalList.length)]
 
-        for( var j = 0; j < animal.length; j++){
-            
-            lettersInAnimal.push({letter: animal[j], showletter:false })
+
+
+        var correct = []
+        for (i = 0; i < animal.length; i++) {
+            correct[i] = "_";
         }
+       alert(correct)
+        // animal2 = animal.replace(new RegExp(".", 'g'), "-");
+        alert(animal);
+        // alert(animal2);
 
-        for ( var k = 0; k < lettersInAnimal.length; k++){
-            if (lettersInAnimal[k].showletter === false){
-                word.innerHTML = word.innerHTML + " _ "
-                
-            }
-            else {
-                word.innerHTML = word.innerHTML + lettersInAnimal[k].letter
-            }
-
-        }
-       
-
-    
 
         document.querySelector(".hidden-word").innerHTML = correct;
 
@@ -70,15 +48,43 @@ document.onkeyup = function (e) {
             alert(animaltwo);
 
 
+              
+
+            // result = function () {
+            //     wordHolder = document.getElementById('hold');
+            //     correct = document.createElement('ul');
+
+            // for (var i = 0; i < word.length; i++) {
+            //   correct.setAttribute('id', 'my-word');
+            //   guess = document.createElement('li');
+            //   guess.setAttribute('class', 'guess');
+            //   if (word[i] === "-") {
+            //     guess.innerHTML = "-";
+            //     space = 1;
+            //   } else {
+            //     guess.innerHTML = "_";
+            //   }
+
+            //   geusses.push(guess);
+            //   wordHolder.appendChild(correct);
+            //   correct.appendChild(guess);
+            // }
+
+
+
+
+
 
 
         } else {
             alert("boo");
-
+       
             var wrong = document.querySelector(".incorrect-guesses").innerHTML = "Wrong Letters: " + userKey;
 
-           
-
+                
+                    
+                    
+                 
 
 
             if (incorrect > 0) {
@@ -86,7 +92,7 @@ document.onkeyup = function (e) {
                 document.querySelector(".numberofguesses").innerHTML = "Chances Left: " + incorrect--;
 
 
-
+                
             } else {
                 alert("Game Over!");
 
@@ -96,6 +102,6 @@ document.onkeyup = function (e) {
         }
 
 
-
+        
     }
 }
